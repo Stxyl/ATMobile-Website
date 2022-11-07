9bb991b63a866480d4fc8616162f7bd159bf64cd
Stxyl <68010279+Stxyl@users.noreply.github.com>
2022-11-08 06:17:14 +1300

    Added Home.js
    Added Home.js which is considered index.html but within React All information on how to edit it is within Home.js

---
+++ atmobile-website/src/pages/Home.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+class Home extends React.Component {
+  render() {
+    return (
+        //edit HTML code here
+        //If you need to edit CSS, go to src/App.css
+        //If you need to edit JavaScript, go to src/App.js
+        //Happy Coding!
+
+        <main>
+          <div className={"Placeholder"}>
+            <h1>Home</h1>
+          </div>
+        </main>
+
+    );
+  }
+}
+
+export default Home;
