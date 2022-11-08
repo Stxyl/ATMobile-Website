3fd7975ffbf55ef737eb745c71dda3d392f9fdc4
Stxyl <68010279+Stxyl@users.noreply.github.com>
2022-11-09 00:02:10 +1300

    Revert "Header & Footer Components"
    This reverts commit f2eb1b8b08d932e13747d1e2cd01007c28de0333.
    
    # Conflicts:
    #	atmobile-website/src/components/Footer.js
    #	atmobile-website/src/components/Header.js
    #	atmobile-website/src/index.css

--- atmobile-website/src/App.js
+++ atmobile-website/src/App.js
@@ -1,17 +1,11 @@
 import Home from './pages/Home';
-import Header from "./components/Header";
-import Footer from "./components/Footer";
-
 
 import './App.css';
-
 
 function App() {
   return (
     <div>
-        <Header />
-        <Home />
-        <Footer />
+      <Home />
     </div>
   );
 }
--- atmobile-website/src/pages/Home.js
+++ atmobile-website/src/pages/Home.js
@@ -4,8 +4,8 @@ class Home extends React.Component {
   render() {
     return (
         //edit HTML code here
-        //If you need to edit CSS, go to src/index.css
-        //If you need to edit JavaScript, go to src/index.js
+        //If you need to edit CSS, go to src/App.css
+        //If you need to edit JavaScript, go to src/App.js
         //Happy Coding!
 
         <main>
