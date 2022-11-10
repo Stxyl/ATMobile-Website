d21fb0cdbfeca9b97e1dcfdf98a7d2467c85d310
dingo-t <101374201+dingo-t@users.noreply.github.com>
2022-11-11 00:15:34 +1300

    structure and class changes
    Container structure improved to aid with future changes to responsiveness.

--- atmobile-website/src/pages/Home.js
+++ atmobile-website/src/pages/Home.js
@@ -10,18 +10,14 @@ class Home extends React.Component {
 
         <main>
           <title>AT APP</title>
-          <div className={"flex-container"}>
+          <div className={"flexcontainer"}>
           
-            <div className={"main-text-cont"}>
             <h1 id="title"> AT Mobile </h1> 
             <h2 id="subtitle">All New, All Redesigned</h2> 
             
-            <div className={"appstore"}>
-            </div> <img src='https://i.postimg.cc/kMR9Mr93/Download-on-the-App-Store-Badge-US-UK-blk-092917.png' alt="download on the app store" width={300} height={100}></img> </div>
-            
-            <div className={"image-cont"}> 
-            <img src='https://i.postimg.cc/P5xVfLF5/Untitled-1.png' alt="screenshot of the app" width={450} height={900}></img>
-          </div>
+            <img className='appstore' src='https://i.postimg.cc/kMR9Mr93/Download-on-the-App-Store-Badge-US-UK-blk-092917.png' alt="download on the app store" width={300} height={100}></img> 
+           
+            <img className='appscreenshot' src='https://i.postimg.cc/P5xVfLF5/Untitled-1.png' alt="screenshot of the app" ></img>
           </div>
         </main>
 
