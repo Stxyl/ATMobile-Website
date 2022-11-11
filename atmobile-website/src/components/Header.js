fa5f75f8480bb03e349b025aa3b1e7868a6169c5
dingo-t <101374201+dingo-t@users.noreply.github.com>
2022-11-12 02:04:15 +1300

    New section and some container changes

--- atmobile-website/src/components/Header.js
+++ atmobile-website/src/components/Header.js
@@ -9,11 +9,11 @@ class Header extends React.Component {
 
             <header>
         
-            <div className={"logo"}>
+            <div className='logo'>
             <img src="https://yt3.ggpht.com/NFB9TV7mnQGxmZsTBt1x_5v0n84M7P6THXRtCy4XvRn7EtslzgakQuh4wqrYH6_r5LWGxnL4lsEfTQ=s640-c-fcrop64=1,00210000ffdeffff-nd-v1" alt="logo"></img>
             </div>
 
-            <div className={"menu"}>
+            <div className='menu'>
                 
                 <h2> = </h2>
             
--- atmobile-website/src/pages/Home.js
+++ atmobile-website/src/pages/Home.js
@@ -10,14 +10,23 @@ class Home extends React.Component {
 
         <main>
           <title>AT APP</title>
-          <div className={"flexcontainer"}>
-          
-            <h1 id="title"> AT Mobile </h1> 
-            <h2 id="subtitle">All New, All Redesigned</h2> 
+          <div className='flexcontainer'>
             
-            <img className='appstore' src='https://i.postimg.cc/kMR9Mr93/Download-on-the-App-Store-Badge-US-UK-blk-092917.png' alt="download on the app store" width={300} height={100}></img> 
-           
-            <img className='appscreenshot' src='https://i.postimg.cc/P5xVfLF5/Untitled-1.png' alt="screenshot of the app" ></img>
+            <div className='textcont'>
+            
+              <h1 className='title'> AT Mobile </h1> 
+              <h2 className='subtitle'> All New, All Redesigned </h2> 
+            
+              <img className='appstore' src='https://i.postimg.cc/kMR9Mr93/Download-on-the-App-Store-Badge-US-UK-blk-092917.png' alt="download on the app store" width={300} height={100}></img> 
+            
+            </div>
+            <div className='appcont'>
+              <img className='appscreenshot' src='https://i.postimg.cc/P5xVfLF5/Untitled-1.png' alt="screenshot of the app" ></img>
+            </div>
+          </div>
+                   
+          <div className='whiteband'>
+
           </div>
         </main>
 
