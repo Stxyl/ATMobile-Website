4a628a2982096ad3148c02da5513c5bbc51ff071
dingo-t <101374201+dingo-t@users.noreply.github.com>
2022-11-16 18:32:50 +1300

    Responsive changes and Menu

--- atmobile-website/src/components/Header.js
+++ atmobile-website/src/components/Header.js
@@ -19,10 +19,16 @@ class Header extends React.Component {
             <img src="https://yt3.ggpht.com/NFB9TV7mnQGxmZsTBt1x_5v0n84M7P6THXRtCy4XvRn7EtslzgakQuh4wqrYH6_r5LWGxnL4lsEfTQ=s640-c-fcrop64=1,00210000ffdeffff-nd-v1" alt="logo"></img>
             </div>
 
-            <div className='menu'>
-                <img src={line3horizontal} alt="menu" />
+            
+            <div class="dropdown">
+                <button class="dropbtn"><img id='sus'src={line3horizontal} alt="menu" /></button>
+                <div class="dropdown-content">
+                    <a href="#">Home</a>
+                    <a href="#">Features</a>
+                    <a href="#">About us</a>
+                    <a href="#">Contact</a>
+                </div>
             </div>
-
             </header>
 
         );
